3ac91388feafad11322cdec16de4bb7dcab7ad9a
wcdk200 <sebguld@gmail.com>
2022-06-02 09:13:20 +0200

    rettelser

--- save_user.py
+++ save_user.py
@@ -4,60 +4,12 @@ import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import mysql.connector
 
-L1 = 24 #25
-L2 = 18 #8
-L3 = 17 #7
-L4 = 1
-
-C1 = 12
-C2 = 16
-C3 = 20
-C4 = 21
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(L1, GPIO.OUT)
-GPIO.setup(L2, GPIO.OUT)
-GPIO.setup(L3, GPIO.OUT)
-GPIO.setup(L4, GPIO.OUT)
 
-# Make sure to configure the input pins to use the internal pull-down resistors
-
-GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-
-def readLine(line, characters):
-    GPIO.output(line, GPIO.HIGH)
-    if(GPIO.input(C1) == 1):
-        print(characters[0])
-    if(GPIO.input(C2) == 1):
-        print(characters[1])
-    if(GPIO.input(C3) == 1):
-        print(characters[2])
-    if(GPIO.input(C4) == 1):
-        print(characters[3])
-    GPIO.output(line, GPIO.LOW)
-
-class keypad:
-  try:
-    while True:
-        # call the readLine function for each row of the keypad
-        readLine(L1, ["1","2","3","A"])
-        readLine(L2, ["4","5","6","B"])
-        readLine(L3, ["7","8","9","C"])
-        readLine(L4, ["*","0","#","D"])
-        time.sleep(0.1)
-  except KeyboardInterrupt:
-    print("\nApplication stopped!")
-
-print("test3")
-GPIO.setwarnings(False)
 db = mysql.connector.connect(
-  host="localhost",
-  user="root",
+  host="192.168.10.47",
+  user="mysqldb",
   passwd="fedtfirma42",
   database="rfiddb"
 )
